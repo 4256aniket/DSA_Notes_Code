e0131b5ed3792ca2a63f7585c381d1c36a973353
Aniket Kumar <aniketkumar@Anikets-MacBook-Air.local>
2023-05-12 20:01:34 +0530

    rooting a tree

--- Graph/algos/graph_input_dfs_bfs.cpp
+++ Graph/algos/graph_input_dfs_bfs.cpp
@@ -1,24 +1,72 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void dfs(vector<vector<int>>& g, int s, vector<bool>& vis) {
-    vis[s] = true;
-    cout << s << " ";
-    for (auto adj : g[s]) {
-        if (!vis[adj]) dfs(g, adj, vis);
+class TreeNode
+{
+public:
+    int val;
+    vector<TreeNode*> children;
+    TreeNode(int nodeVal)
+    {
+        this->val = nodeVal;
+    }
+};
+
+void printTree(TreeNode *root)
+{
+    cout << root->val << " : ";
+    for (auto child : root->children) {
+        cout << child->val << " ";
+    }
+    cout << endl;
+    for (auto child : root->children) {
+        printTree(child);
     }
 }
 
-void bfs(vector<vector<int>>& g, int s, vector<bool>& vis) {
+TreeNode* buildTree(vector<vector<int>> &g, TreeNode *node, int parent)
+{
+    for (auto childId : g[node->val])
+    {
+        if (childId == parent) continue;
+        TreeNode *child = new TreeNode(childId);
+        node->children.push_back(child);
+        buildTree(g, child, node->val);
+    }
+    return node;
+}
+
+TreeNode* rootTree(vector<vector<int>> &g, int rootId = 0)
+{
+    TreeNode *root = new TreeNode(rootId);
+    return buildTree(g, root, -1);
+}
+
+void dfs(vector<vector<int>> &g, int s, vector<bool> &vis)
+{
+    vis[s] = true;
+    cout << s << " ";
+    for (auto adj : g[s])
+    {
+        if (!vis[adj])
+            dfs(g, adj, vis);
+    }
+}
+
+void bfs(vector<vector<int>> &g, int s, vector<bool> &vis)
+{
     queue<int> q;
     q.push(s);
     vis[s] = true;
-    while (!q.empty()) {
+    while (!q.empty())
+    {
         int node = q.front();
         q.pop();
         cout << node << " ";
-        for (auto adj : g[node]) {
-            if (!vis[adj]) {
+        for (auto adj : g[node])
+        {
+            if (!vis[adj])
+            {
                 q.push(adj);
                 vis[adj] = true;
             }
@@ -26,22 +74,26 @@ void bfs(vector<vector<int>>& g, int s, vector<bool>& vis) {
     }
 }
 
-vector<vector<int>> TakeInput(bool undirected = true) {
+vector<vector<int>> TakeInput(bool undirected = true)
+{
     int V, E;
     cin >> V >> E;
     vector<vector<int>> g(V);
-    for (int i = 0; i < E; i++) {
+    for (int i = 0; i < E; i++)
+    {
         int u, v;
         cin >> u >> v;
         g[u].push_back(v);
-        if (!undirected) continue;
+        if (!undirected)
+            continue;
         g[v].push_back(u);
     }
 
     return g;
 }
 
-int main() {
+int main()
+{
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
@@ -49,10 +101,14 @@ int main() {
 
     vector<vector<int>> g = TakeInput();
     vector<bool> vis(g.size(), false);
-    for(int i = 0; i < g.size(); i++){
-        if(!vis[i]){
-            bfs(g,i,vis);
-        }
-    }
-
+    // for (int i = 0; i < g.size(); i++)
+    // {
+    //     if (!vis[i])
+    //     {
+    //         bfs(g, i, vis);
+    //     }
+    // }
+    TreeNode *root = new TreeNode(0);
+    root = rootTree(g);
+    printTree(root);
 }
