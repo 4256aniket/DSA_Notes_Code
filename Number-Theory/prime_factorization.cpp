8a9dcfc4d1db7b257b2f5d2f8aa1b9ca0a39d950
Aniket Kumar <aniketkumar@Anikets-MacBook-Air.local>
2023-05-27 00:43:29 +0530

    prime_factorization

---
+++ Number-Theory/prime_factorization.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+	int n;
+	cin >> n;
+
+	for(int div = 2; div < n; div++){
+		while(n % div == 0){
+			n = n / div;
+			cout << div << endl;
+		}
+	}
+}
